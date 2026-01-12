2e13321811b9c0d61b334e8dfd17bbd396961cbe
Daniel Willoughby <omnical@outlook.com>
2026-01-12 23:57:01 +1000

    category label stylesheet

--- src/components/CyberwareHandler/CategoryHandler/CategoryLabel/index.jsx
+++ src/components/CyberwareHandler/CategoryHandler/CategoryLabel/index.jsx
@@ -1,11 +1,8 @@
+import './CategoryLabel.css'
+
 export default function CategoryLabel({ children, isRight }) {
   return (
-    <div
-      style={{
-        fontSize: '2.5vh',
-        textAlign: isRight ? 'left' : 'right', // TODO: May be possible to convert to css logic line
-      }}
-    >
+    <div className={`category-label ${isRight ? 'right' : 'left'}`}>
       {children}
     </div>
   )
