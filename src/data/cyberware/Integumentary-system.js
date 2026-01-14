18a30fba7810f001483cfd1413114cb846f154de
Daniel Willoughby <omnical@outlook.com>
2026-01-14 17:20:12 +1000

    temp data

---
+++ src/data/cyberware/Integumentary-system.js
@@ -0,0 +1,20 @@
+export const integumentarySystem = [
+  {
+    id: '0',
+    label: 'Item A',
+    capacity: 10,
+    conflicts: [],
+  },
+  {
+    id: '1',
+    label: 'Item B',
+    capacity: 20,
+    conflicts: [],
+  },
+  {
+    id: '2',
+    label: 'Item C',
+    capacity: 30,
+    conflicts: [],
+  },
+]
--- src/data/cyberware/index.js
+++ src/data/cyberware/index.js
@@ -1,5 +1,23 @@
-import { frontalCortex } from './frontal-cortex.js'
+import { frontalCortex } from './frontal-cortex'
+import { operatingSystem } from './operating-system'
+import { arms } from './arms'
+import { face } from './face'
+import { skeleton } from './skeleton'
+import { hands } from './hands'
+import { nervousSystem } from './nervous-system'
+import { circulatorySystem } from './circulatory-system'
+import { integumentarySystem } from './integumentary-system'
+import { legs } from './legs'
 
 export const cyberware = {
-  'frontal-cortex': frontalCortex,
+  frontalCortex: frontalCortex,
+  operatingSystem: operatingSystem,
+  arms: arms,
+  face: face,
+  skeleton: skeleton,
+  hands: hands,
+  nervousSystem: nervousSystem,
+  circulatorySystem: circulatorySystem,
+  integumentarySystem: integumentarySystem,
+  legs: legs,
 }
