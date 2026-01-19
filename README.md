5f8e8a3b8d26270342411aae6d3f535d57be5c49
Daniel Willoughby <omnical@outlook.com>
2026-01-19 14:27:57 +1000

    validate url params

--- README.md
+++ README.md
@@ -15,4 +15,4 @@ The React Compiler is not enabled on this template because of its impact on dev 
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
 
-http://localhost:5173/?a=a,b,c&b=a&c=a&d=a&e=a,b,c&f=a,b&g=a,b,c&h=a,b,c&i=a,b,c&j=a
+http://localhost:5173/?a=abc&b=a&c=a&d=a&e=abc&f=ab&g=abc&h=abc&i=abc&j=a
--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -1,17 +1,28 @@
-import { useMemo } from 'react'
+const defaultSearch = '?a=&b=&c=&d=&e=&f=&g=&h=&i=&j=' // TODO: create from array of letters
+const defaultSearchParams = new URLSearchParams(defaultSearch)
+const defaultSearchKeys = Array.from(defaultSearchParams.keys())
+
+function readSearchParams(searchParams) {
+  const result = {}
+  for (const [key, value] of searchParams) {
+    result[key] = value.split('')
+  }
+  return result
+}
+
+function arraysEqual(a, b) {
+  return a.length === b.length && a.every((v, i) => v === b[i])
+}
 
 export function useData() {
-  return useMemo(() => {
-    const params = new URLSearchParams(window.location.search)
-    const result = {}
+  const searchParams = new URLSearchParams(window.location.search) // https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+  const urlKeys = Array.from(searchParams.keys())
 
-    for (const [key, value] of params.entries()) {
-      result[key] = value
-        .split(',')
-        .map((v) => decodeURIComponent(v.trim()))
-        .filter(Boolean)
-    }
+  // TODO: validate ids in categories
+  if (!arraysEqual(urlKeys, defaultSearchKeys)) {
+    window.history.replaceState(null, '', defaultSearch)
+    return readSearchParams(defaultSearchParams)
+  }
 
-    return result
-  }, [])
+  return readSearchParams(searchParams)
 }
