e2729926b2631044db0d9bffa62f3a9f90ab7b4b
Daniel Willoughby <omnical@outlook.com>
2026-01-20 15:31:14 +1000

    show equipped and equippable

--- src/components/OptionsHandler/index.jsx
+++ src/components/OptionsHandler/index.jsx
@@ -1,26 +1,29 @@
 import ListLayout from '../ListLayout'
 import GridLayout from '../GridLayout'
-import TempBox from '../TempBox'
 import { usePage } from '../../contexts/PageContext'
 import { useData } from '../../hooks/useData'
 
 import { categoriesLookUp } from '../../data/categories'
-import { cyberwareLookUp } from '../../data/cyberware'
+import { cyberware, cyberwareLookUp } from '../../data/cyberware'
 import SlotButton from '../SlotButton/index.jsx'
 
 export default function OptionsHandler() {
   const { page, setPage } = usePage()
 
   const urlData = useData()
-  const category = categoriesLookUp[page]
+  const categoryInfo = categoriesLookUp[page]
+
+  const filteredCyberware = cyberware[page].filter(
+    (item) => !urlData[page].includes(item.id),
+  )
 
   return (
     <>
       <div className={'options-container'}>
-        <div className={'category-selector'}>{category.label}</div>
+        <div className={'category-selector'}>{categoryInfo.label}</div>
         <ListLayout gap={'1.5vh'} className={'equipped-container'}>
-          {Array.from({ length: category.slots }).map((_, index) => {
-            const item = cyberwareLookUp[page][urlData[page][index]] // TODO: may be better to pass the data of each equipped item into this component instead of handling data here
+          {Array.from({ length: categoryInfo.slots }).map((_, index) => {
+            const item = cyberwareLookUp[page][urlData[page][index]] // TODO: use cyberware.find instead?
 
             return (
               <SlotButton key={index}>{item ? item.label : 'Empty'}</SlotButton>
@@ -33,14 +36,9 @@ export default function OptionsHandler() {
           columnGap={'1.5vh'}
           className={'items-container'}
         >
-          <TempBox />
-          <TempBox />
-          <TempBox />
-          <TempBox />
-          <TempBox />
-          <TempBox />
-          <TempBox />
-          <TempBox />
+          {filteredCyberware.map((item, index) => (
+            <SlotButton key={index}>{item ? item.label : 'Empty'}</SlotButton>
+          ))}
         </GridLayout>
       </div>
       <button onClick={() => setPage('MainMenu')}>Back</button>
