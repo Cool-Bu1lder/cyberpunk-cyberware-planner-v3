195cf5a8d57359e38d2e68fccf0947f735c159a8
Daniel Willoughby <omnical@outlook.com>
2026-01-20 12:51:04 +1000

    page context cleanup

--- src/contexts/PageContext.jsx
+++ src/contexts/PageContext.jsx
@@ -1,10 +1,14 @@
+// https://react.dev/learn/scaling-up-with-reducer-and-context
+
 import { createContext, useContext, useState } from 'react'
 
 const PageContext = createContext(null)
 
+const initialPage = 'MainMenu'
+
 export function PageProvider({ children }) {
-  const [page, setPage] = useState('MainMenu')
-  return <PageContext value={{page, setPage}}>{children}</PageContext>
+  const [page, setPage] = useState(initialPage)
+  return <PageContext value={{ page, setPage }}>{children}</PageContext>
 }
 
 export function usePage() {
