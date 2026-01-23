f2ea2d70c29bf9fb15680cb92837a1d6450a0ae7
Daniel Willoughby <omnical@outlook.com>
2026-01-23 22:59:08 +1000

    dynamic vite config for development

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+// https://stackoverflow.com/a/79656173
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/',
-})
+  base: mode === 'development' ? '/' : '/cyberpunk-cyberware-planner-v3/',
+}))
