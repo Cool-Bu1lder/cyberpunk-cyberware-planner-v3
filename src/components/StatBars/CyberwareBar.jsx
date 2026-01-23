0c72f81e17198a5ecbf7954f3c481666f54018a8
Daniel Willoughby <omnical@outlook.com>
2026-01-23 21:36:11 +1000

    cyberware bar

--- src/components/StatBars/CyberwareBar.jsx
+++ src/components/StatBars/CyberwareBar.jsx
@@ -11,15 +11,18 @@ export default function CyberwareBar({ now, label }) {
   return (
     <div className="bar-container left vertically-centered">
       <div>{label}</div>
-      <div className="tick-set">
+      <div className="tick-groups-layout">
         {Array.from({ length: groups }).map((_, i) => (
-          <div className="tick-group">
-            {Array.from({ length: bars }).map((_, j) => (
-              <div className="tick-row">
-                <div className="tick" />
-                <div className="tick" />
-              </div>
-            ))}
+          <div className="tick-row-layout">
+            {Array.from({ length: bars }).map((_, j) => {
+              const active = fill > j + i * bars
+              return (
+                <div className="tick-layout">
+                  <div className={`tick ${active ? 'active' : ''}`} />
+                  <div className={`tick ${active ? 'active' : ''}`} />
+                </div>
+              )
+            })}
           </div>
         ))}
       </div>
