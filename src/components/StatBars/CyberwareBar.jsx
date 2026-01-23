bba6c6f3d7a8144284de602cc0034cc5128f8326
Daniel Willoughby <omnical@outlook.com>
2026-01-23 22:14:07 +1000

    cyberware bar animation

--- src/components/StatBars/CyberwareBar.jsx
+++ src/components/StatBars/CyberwareBar.jsx
@@ -16,10 +16,14 @@ export default function CyberwareBar({ now, label }) {
           <div className="tick-row-layout">
             {Array.from({ length: bars }).map((_, j) => {
               const active = fill > j + i * bars
+              const activeClass = `tick ${active ? 'active' : ''}`
+              const p = fill - j - i * bars - 1
+              const positionClass =
+                p === 0 ? 'first' : p === 1 ? 'second' : p === 2 ? 'third' : ''
               return (
                 <div className="tick-layout">
-                  <div className={`tick ${active ? 'active' : ''}`} />
-                  <div className={`tick ${active ? 'active' : ''}`} />
+                  <div className={`${activeClass}`} />
+                  <div className={`${activeClass} ${positionClass}`} />
                 </div>
               )
             })}
