f0087b08cef5a04d67337d416cc8e0698ae04bab
Daniel Willoughby <omnical@outlook.com>
2026-01-28 14:42:20 +1000

    cyberware bar cleanup 1

--- src/components/StatBars/CyberwareBar.jsx
+++ src/components/StatBars/CyberwareBar.jsx
@@ -7,8 +7,35 @@ const bars = 10
 
 const total = groups * bars
 
+function TickRow({ fill, j, groupIndex }) {
+  const active = fill > j + groupIndex * bars
+  const activeClass = `tick ${active ? 'active' : ''}`
+  const p = fill - j - groupIndex * bars - 1
+  const positionClass =
+    p === 0 ? 'first' : p === 1 ? 'second' : p === 2 ? 'third' : ''
+
+  return (
+    <div className="tick-layout">
+      <div className={`${activeClass}`} />
+      <div className={`${activeClass} ${positionClass}`} />
+    </div>
+  )
+}
+
+function TickGroup({ groupIndex, fill }) {
+  const tickRow = Array.from({ length: bars }).map((_, j) => (
+    <TickRow fill={fill} j={j} groupIndex={groupIndex} />
+  ))
+
+  return <div className="tick-row-layout">{tickRow}</div>
+}
+
 export default function CyberwareBar({ now, label }) {
   const fill = Math.floor((now / 100) * total)
+
+  const tickGroups = Array.from({ length: groups }).map((_, i) => (
+    <TickGroup group={i} fill={fill} />
+  ))
 
   return (
     <div className="bar-container left vertically-centered">
@@ -16,25 +43,7 @@ export default function CyberwareBar({ now, label }) {
         {label}
       </div>
       <img src={viteLogo} className="logo" alt="Vite logo" />
-      <div className="tick-groups-layout">
-        {Array.from({ length: groups }).map((_, i) => (
-          <div className="tick-row-layout">
-            {Array.from({ length: bars }).map((_, j) => {
-              const active = fill > j + i * bars
-              const activeClass = `tick ${active ? 'active' : ''}`
-              const p = fill - j - i * bars - 1
-              const positionClass =
-                p === 0 ? 'first' : p === 1 ? 'second' : p === 2 ? 'third' : ''
-              return (
-                <div className="tick-layout">
-                  <div className={`${activeClass}`} />
-                  <div className={`${activeClass} ${positionClass}`} />
-                </div>
-              )
-            })}
-          </div>
-        ))}
-      </div>
+      <div className="tick-groups-layout">{tickGroups}</div>
     </div>
   )
 }
