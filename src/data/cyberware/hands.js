05a4f8482c0190968606bdb63999fcd0ee338c6d
Daniel Willoughby <omnical@outlook.com>
2026-02-02 15:12:32 +1000

    hands

--- src/data/cyberware/hands.js
+++ src/data/cyberware/hands.js
@@ -1,20 +1,67 @@
+import { icons } from '../icons.js'
+
 export const hands = [
   {
     id: 'a',
-    label: 'Item A',
-    capacity: 10,
+    label: 'Ballistic Coprocessor',
+    capacity: 2,
+    icon: icons['hands']['PowerGrip'],
     conflicts: [],
   },
   {
     id: 'b',
-    label: 'Item B',
-    capacity: 20,
+    label: 'Handle Wrap',
+    capacity: 8,
+    icon: icons['hands']['KnifeSharpener'],
     conflicts: [],
   },
   {
     id: 'c',
-    label: 'Item C',
-    capacity: 30,
+    label: 'Immovable Force',
+    capacity: 35,
+    icon: icons['hands']['GunStabilizer'],
+    conflicts: [],
+  },
+  {
+    id: 'd',
+    label: 'Microgenerator',
+    capacity: 9,
+    icon: icons['hands']['MicroGenerator'],
+    conflicts: [],
+  },
+  {
+    id: 'e',
+    label: 'Shock Absorber',
+    capacity: 12,
+    icon: icons['hands']['JointLock'],
+    conflicts: [],
+  },
+  {
+    id: 'f',
+    label: 'Smart Link',
+    capacity: 4,
+    icon: icons['hands']['SmartLink'],
+    conflicts: [],
+  },
+  {
+    id: 'g',
+    label: "Tattoo: Johnny's Special",
+    capacity: 0,
+    icon: icons['hands']['CasiusTattoo'],
+    conflicts: [],
+  },
+  {
+    id: 'h',
+    label: 'Tattoo: Together Forever',
+    capacity: 0,
+    icon: icons['hands']['SilverhandTattoo'],
+    conflicts: [],
+  },
+  {
+    id: 'i',
+    label: 'Tattoo: Tyger Claws Dermal Imprint',
+    capacity: 0,
+    icon: icons['hands']['YakuzaTattoo'],
     conflicts: [],
   },
 ]
