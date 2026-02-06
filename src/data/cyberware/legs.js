92b3f9c7a02b76b3502168d870ad6f220356de4f
Daniel Willoughby <omnical@outlook.com>
2026-02-06 22:25:17 +1000

    legs

--- src/data/cyberware/legs.js
+++ src/data/cyberware/legs.js
@@ -1,20 +1,43 @@
+import { icons } from '../icons.js'
+
 export const legs = [
   {
     id: 'a',
-    label: 'Item A',
-    capacity: 10,
+    label: 'Fortified Ankles',
+    capacity: 6,
+    icon: icons['legs']['ReinforcedMuscles'],
+    armor: 103,
     conflicts: [],
   },
   {
     id: 'b',
-    label: 'Item B',
-    capacity: 20,
+    label: "Jenkins' Tendons",
+    capacity: 6,
+    icon: icons['legs']['JenkinsTendons'],
+    armor: 22,
     conflicts: [],
   },
   {
     id: 'c',
-    label: 'Item C',
-    capacity: 30,
+    label: 'Leeroy Ligament System',
+    capacity: 8,
+    icon: icons['legs']['IconicJenkinsTendons'],
+    armor: 26,
+    conflicts: [],
+  },
+  {
+    id: 'd',
+    label: 'Lynx Paws',
+    capacity: 5,
+    icon: icons['legs']['CatPaws'],
+    armor: 34,
+    conflicts: [],
+  },
+  {
+    id: '3',
+    label: 'Reinforced Tendons',
+    capacity: 8,
+    icon: icons['legs']['BoostedTendons'],
     conflicts: [],
   },
 ]
