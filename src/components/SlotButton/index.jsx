d5e7baba8905f8ef3ffedb439284b57c667ff32d
Daniel Willoughby <omnical@outlook.com>
2026-02-10 00:01:08 +1000

    empty icon

--- src/components/SlotButton/index.jsx
+++ src/components/SlotButton/index.jsx
@@ -1,13 +1,13 @@
 import './SlotButton.css'
 
-import viteLogo from '../../assets/vite.svg'
+import emptyIcon from '../../assets/empty.webp'
 
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_icon_buttons
 
 export default function SlotButton({ children, className, onClick, icon }) {
   return (
     <button className={`slot-button ${className}`} onClick={onClick} style={{}}>
-      <img src={icon || viteLogo} className={'icon'} alt={children} />
+      <img src={icon || emptyIcon} className={'icon'} alt={children} />
       {children}
     </button>
   )
