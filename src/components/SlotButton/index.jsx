62899bc7dc2da7eab2c4a3157bafdd5f64dbfd6e
Daniel Willoughby <omnical@outlook.com>
2026-02-10 00:17:15 +1000

    slot button styling

--- src/components/SlotButton/index.jsx
+++ src/components/SlotButton/index.jsx
@@ -5,8 +5,14 @@ import emptyIcon from '../../assets/empty.webp'
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_icon_buttons
 
 export default function SlotButton({ children, className, onClick, icon }) {
+  const colour = icon ? 'full' : 'empty'
+
   return (
-    <button className={`slot-button ${className}`} onClick={onClick} style={{}}>
+    <button
+      className={`slot-button ${colour} ${className}`}
+      onClick={onClick}
+      style={{}}
+    >
       <img src={icon || emptyIcon} className={'icon'} alt={children} />
       {children}
     </button>
