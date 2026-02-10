4bff6390f2a8a9a5ef51a8a9ba4d050397c18706
Daniel Willoughby <omnical@outlook.com>
2026-02-10 22:25:28 +1000

    animation

--- src/components/ModelView/Humanoid.jsx
+++ src/components/ModelView/Humanoid.jsx
@@ -1,9 +1,23 @@
 import { useLoader } from '@react-three/fiber'
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js'
+import { useAnimations } from '@react-three/drei'
 
 import characterModel from '../../assets/Y Bot.fbx?url'
+import idleAnimation from '../../assets/y-bot-idle.fbx?url'
+import { useEffect } from 'react'
 
 export default function Humanoid(props) {
   const fbx = useLoader(FBXLoader, characterModel)
+  const { animations: idleClips } = useLoader(FBXLoader, idleAnimation)
+
+  const { actions, names } = useAnimations(idleClips, fbx)
+
+  useEffect(() => {
+    const animationName = names[0]
+    if (actions[animationName]) {
+      actions[animationName].reset().fadeIn(0).play()
+    }
+  }, [actions, names])
+
   return <primitive {...props} object={fbx} />
 }
