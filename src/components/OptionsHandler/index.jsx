909b3bb73b56db9e119bb8e239f2f73b763252dd
Daniel Willoughby <daniel.willoughby@connect.qut.edu.au>
2026-02-14 12:36:36 +1000

    separated options handler into components, information debug view on hover for equipped items

--- src/components/OptionsHandler/index.jsx
+++ src/components/OptionsHandler/index.jsx
@@ -8,30 +8,53 @@ import { categories } from '../../data/categories'
 import { categoriesLookUp } from '../../data/categories'
 import { cyberware, cyberwareLookUp } from '../../data/cyberware'
 import SlotButton from '/src/components/SlotButton/SlotButton'
+import { useState } from 'react'
 
 import './OptionsHandler.css'
 
-export default function OptionsHandler() {
+function CategorySelector() {
   const { page, setPage } = usePage()
+
+  const categoryInfo = categoriesLookUp[page]
+
+  function changePage(increment) {
+    const i = categories.findIndex((item) => item.id === page) + increment
+    const n = categories.length
+    const p = categories[((i % n) + n) % n].id // arr[ (i % n + n) % n ]
+    setPage(p)
+  }
+
+  return (
+    <div className={'category-selector'}>
+      <button
+        className={'category-button left'}
+        onClick={() => changePage(-1)}
+      ></button>
+      <div className={'category-divider'}>
+        <div className={'option-label'}>{categoryInfo.label}</div>
+        <div className={'c-sm'}>
+          {Array.from({ length: categories.length }).map((_, i) => {
+            const j = categories.findIndex((item) => item.id === page)
+            const active = i === j ? 'active' : ''
+            return <div className={`i-bar ${active}`} />
+          })}
+        </div>
+      </div>
+      <button
+        className={'category-button right'}
+        onClick={() => changePage(1)}
+      ></button>
+    </div>
+  )
+}
+
+function EquippedItems() {
+  const { page } = usePage()
 
   const { data, setData } = useData()
   const categoryInfo = categoriesLookUp[page]
 
-  const filteredCyberware = cyberware[page].filter(
-    (item) => !data[page].includes(item.id),
-  )
-
-  function equip(itemId) {
-    const searchParams = new URLSearchParams(window.location.search)
-    const currentValues = searchParams.get(page)
-
-    if (currentValues.length >= categoryInfo.slots) return null
-
-    searchParams.set(page, currentValues + itemId)
-    window.history.replaceState(null, '', '?' + searchParams.toString())
-
-    setData(() => getData())
-  }
+  const [activeIndex, setActiveIndex] = useState(-1)
 
   function unequip(itemId) {
     const searchParams = new URLSearchParams(window.location.search)
@@ -45,67 +68,89 @@ export default function OptionsHandler() {
     setData(() => getData())
   }
 
-  function changePage(increment) {
-    const i = categories.findIndex((item) => item.id === page) + increment
-    const n = categories.length
-    const p = categories[((i % n) + n) % n].id // arr[ (i % n + n) % n ]
-    setPage(p)
+  return (
+    <ListLayout gap={'1.5vh'} className={'equipped-container'}>
+      {Array.from({ length: categoryInfo.slots }).map((_, index) => {
+        const item = cyberwareLookUp[page][data[page][index]] // TODO: use cyberware.find instead?
+
+        return (
+          <SlotButton
+            key={index}
+            onClick={() => unequip(data[page][index])}
+            onMouseEnter={() => {
+              setActiveIndex(index)
+            }}
+            onMouseLeave={() => {
+              setActiveIndex(-1)
+            }}
+            icon={item ? item.icon : null}
+          >
+            {activeIndex === index ? (
+              <div>
+                {item ? item.label : 'Empty'}
+                {item ? item.capacity : ''}
+                {item ? item.armor : ''}
+              </div>
+            ) : null}
+          </SlotButton>
+        )
+      })}
+    </ListLayout>
+  )
+}
+
+function ItemInventory() {
+  const { page } = usePage()
+
+  const { data, setData } = useData()
+  const categoryInfo = categoriesLookUp[page]
+
+  const filteredCyberware = cyberware[page].filter(
+    (item) => !data[page].includes(item.id),
+  )
+
+  const [activeIndex, setActiveIndex] = useState(-1)
+
+  function equip(itemId) {
+    const searchParams = new URLSearchParams(window.location.search)
+    const currentValues = searchParams.get(page)
+
+    if (currentValues.length >= categoryInfo.slots) return null
+
+    searchParams.set(page, currentValues + itemId)
+    window.history.replaceState(null, '', '?' + searchParams.toString())
+
+    setData(() => getData())
   }
 
   return (
+    <GridLayout
+      columns={4}
+      rowGap={'1.5vh'}
+      columnGap={'1.5vh'}
+      className={'items-container'}
+    >
+      {filteredCyberware.map((item, index) => (
+        <SlotButton
+          key={index}
+          onClick={() => equip(item.id)}
+          icon={item ? item.icon : null}
+        >
+          {item ? item.label : 'Empty'}
+        </SlotButton>
+      ))}
+    </GridLayout>
+  )
+}
+
+export default function OptionsHandler() {
+  const { setPage } = usePage()
+  return (
     <>
       <div className={'options-container'}>
-        <div className={'category-selector'}>
-          <button
-            className={'category-button left'}
-            onClick={() => changePage(-1)}
-          ></button>
-          <div className={'category-divider'}>
-            <div className={'option-label'}>{categoryInfo.label}</div>
-            <div className={'c-sm'}>
-              {Array.from({ length: categories.length }).map((_, i) => {
-                const j = categories.findIndex((item) => item.id === page)
-                const active = i === j ? 'active' : ''
-                return <div className={`i-bar ${active}`} />
-              })}
-            </div>
-          </div>
-          <button
-            className={'category-button right'}
-            onClick={() => changePage(1)}
-          ></button>
-        </div>
-        <ListLayout gap={'1.5vh'} className={'equipped-container'}>
-          {Array.from({ length: categoryInfo.slots }).map((_, index) => {
-            const item = cyberwareLookUp[page][data[page][index]] // TODO: use cyberware.find instead?
-
-            return (
-              <SlotButton
-                key={index}
-                onClick={() => unequip(data[page][index])}
-                icon={item ? item.icon : null}
-              >
-                {item ? item.label : 'Empty'}
-              </SlotButton>
-            )
-          })}
-        </ListLayout>
-        <GridLayout
-          columns={4}
-          rowGap={'1.5vh'}
-          columnGap={'1.5vh'}
-          className={'items-container'}
-        >
-          {filteredCyberware.map((item, index) => (
-            <SlotButton
-              key={index}
-              onClick={() => equip(item.id)}
-              icon={item ? item.icon : null}
-            >
-              {item ? item.label : 'Empty'}
-            </SlotButton>
-          ))}
-        </GridLayout>
+        <CategorySelector />
+        <EquippedItems />
+        <ItemInventory />
       </div>
       <button onClick={() => setPage('MainMenu')}>Back</button>
     </>
--- src/components/SlotButton/SlotButton.jsx
+++ src/components/SlotButton/SlotButton.jsx
@@ -4,7 +4,14 @@ import emptyIcon from '../../assets/empty.webp'
 
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_icon_buttons
 
-export default function SlotButton({ children, className, onClick, icon }) {
+export default function SlotButton({
+  children,
+  className,
+  onClick,
+  onMouseEnter,
+  onMouseLeave,
+  icon,
+}) {
   const colour = icon ? styles.full : styles.empty
   const imgSrc = icon ? icon : emptyIcon
 
@@ -12,6 +19,8 @@ export default function SlotButton({ children, className, onClick, icon }) {
     <button
       className={`${styles['slot-button']} ${colour} ${className}`}
       onClick={onClick}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
       style={{}}
     >
       <img src={imgSrc} className={styles.icon} alt={children} />
